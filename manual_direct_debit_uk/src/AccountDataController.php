8d7fb218c1aa520f2660fb3fd2b251e6170cfaf7
Roman Zimmermann <roman@more-onion.com>
2014-10-25 10:30:35 +0200

    Account form for UK transactions.

--- manual_direct_debit_uk/src/AccountDataController.php
+++ manual_direct_debit_uk/src/AccountDataController.php
@@ -9,6 +9,6 @@ class AccountDataController extends \Drupal\manual_direct_debit\AccountDataContr
   public function __construct() {
     parent::__construct();
     $this->title = t('Collect account data (UK)');
-    $this->form = new \Drupal\payment_forms\AccountForm();
+    $this->form = new \Drupal\manual_direct_debit_uk\AccountForm();
   }
 }
