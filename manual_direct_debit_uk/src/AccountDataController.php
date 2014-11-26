8f1da49a2b7041eabd900be99796617383bab6f1
Roman Zimmermann <roman@more-onion.com>
2014-11-26 11:26:18 +0100

    Only show direct debit for recurrent payments.

--- manual_direct_debit_uk/src/AccountDataController.php
+++ manual_direct_debit_uk/src/AccountDataController.php
@@ -2,6 +2,8 @@
 
 namespace Drupal\manual_direct_debit_uk;
 
+use \Drupal\payment_forms\PaymentContextInterface;
+
 class AccountDataController extends \Drupal\manual_direct_debit\AccountDataController {
   /**
    * Define callbacks and classes.
@@ -10,5 +12,18 @@ class AccountDataController extends \Drupal\manual_direct_debit\AccountDataContr
     parent::__construct();
     $this->title = t('Collect account data (UK)');
     $this->form = new \Drupal\manual_direct_debit_uk\AccountForm();
+  }
+
+  function validate(\Payment $payment, \PaymentMethod $payment_method, $strict) {
+    parent::validate($payment, $payment_method, $strict);
+    if (isset($payment->context_data['context'])) {
+      $context = $payment->context_data['context'];
+      if ($context instanceof PaymentContextInterface) {
+        $interval = $context->value('donation_interval');
+        if (!$interval || $interval == '1') {
+          throw new \PaymentValidationException('This payment method does not support one-off payments.');
+        }
+      }
+    }
   }
 }
