d424747642bcd559d135531acea3bb422b56e06d
Roman Zimmermann <roman@more-onion.com>
2015-01-17 15:23:35 +0100

    Move from payment->context_data['context'] to payment->contextObj.

--- manual_direct_debit_uk/src/AccountDataController.php
+++ manual_direct_debit_uk/src/AccountDataController.php
@@ -2,8 +2,6 @@
 
 namespace Drupal\manual_direct_debit_uk;
 
-use \Drupal\payment_forms\PaymentContextInterface;
-
 class AccountDataController extends \Drupal\manual_direct_debit\AccountDataController {
   /**
    * Define callbacks and classes.
@@ -16,14 +14,9 @@ class AccountDataController extends \Drupal\manual_direct_debit\AccountDataContr
 
   function validate(\Payment $payment, \PaymentMethod $payment_method, $strict) {
     parent::validate($payment, $payment_method, $strict);
-    if (isset($payment->context_data['context'])) {
-      $context = $payment->context_data['context'];
-      if ($context instanceof PaymentContextInterface) {
-        $interval = $context->value('donation_interval');
-        if (!$interval || $interval == '1') {
-          throw new \PaymentValidationException('This payment method does not support one-off payments.');
-        }
-      }
+    $interval = $payment->contextObj->value('donation_interval');
+    if (!$interval || $interval == '1') {
+      throw new \PaymentValidationException('This payment method does not support one-off payments.');
     }
   }
 }
