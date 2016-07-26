3cdd874741a2343ed5e26741d46571607697943d
Maya Kokits <hello@mayakokits.com>
2016-07-26 15:48:58 +0200

    prettier code
    - camelCase properties
    - makeRequest returns data
    - call watchdog() instead of watchdog_exception()

--- manual_direct_debit_uk/src/AccountValidation.php
+++ manual_direct_debit_uk/src/AccountValidation.php
@@ -6,70 +6,41 @@ class AccountValidation {
 
   //Credit: Thanks to Stuart Sillitoe (http://stu.so/me) for the original PHP that these samples are based on.
 
-  private $Key; //The key to use to authenticate to the service.
-  private $AccountNumber; //The bank account number to validate.
-  private $SortCode; //The branch sort code for the account number.
-  private $Data; //Holds the results of the query
-  private $Error; //Error information
+  private $key; //The key to use to authenticate to the service.
+  private $accountNumber; //The bank account number to validate.
+  private $sortCode; //The branch sort code for the account number.
 
-  function __construct($Key, $AccountNumber, $SortCode) {
-    $this->Key = $Key;
-    $this->AccountNumber = $AccountNumber;
-    $this->SortCode = $SortCode;
+  function __construct($key, $accountNumber, $sortCode) {
+    $this->key = $key;
+    $this->accountNumber = $accountNumber;
+    $this->sortCode = $sortCode;
   }
 
-  function MakeRequest() {
+  function makeRequest() {
     $url = "https://services.postcodeanywhere.co.uk/BankAccountValidation/Interactive/Validate/v2.00/json.ws?";
-    $url .= "&Key=" . urlencode($this->Key);
-    $url .= "&AccountNumber=" . urlencode($this->AccountNumber);
-    $url .= "&SortCode=" . urlencode($this->SortCode);
+    $url .= "&Key=" . urlencode($this->key);
+    $url .= "&AccountNumber=" . urlencode($this->accountNumber);
+    $url .= "&SortCode=" . urlencode($this->sortCode);
 
     // Make the request to Postcode Anywhere and parse the JSON returned
     $response = drupal_http_request($url, ["timeout" => 5]);
     if ($response->code != 200) {
-      $exception = new \Exception("Could not connect to PCA server");
-      function_exists("watchdog_exception") && watchdog_exception("manual_direct_debit_uk", $exception);
-      return;
+      watchdog("manual_direct_debit_uk", "Could not connect to PCA server", NULL, WATCHDOG_WARNING);
+      return false;
     }
     $json = json_decode($response->data);
     if (!$json) {
-      $exception = new \Exception("Could not parse response data as JSON");
-      function_exists("watchdog_exception") && watchdog_exception("manual_direct_debit_uk", $exception);
-      return;
+      watchdog("manual_direct_debit_uk", "Could not parse response data as JSON", NULL, WATCHDOG_WARNING);
+      return false;
     }
     $data = $json[0];
 
-    // Check for Errors
-    if (isset($data->Error)) {
-      if ($data->Error < 100) {
-        // General errors throw an exception
-        $exception = new \Exception($data->Error);
-        $message = "Error: " . $data->Description . " – "  . $data->Cause . "\n" . $data->Resolution;
-        function_exists("watchdog_exception") && watchdog_exception("manual_direct_debit_uk", $exception, $message);
-      }
-      $this->Error = array(
-        "id" => $data->Error,
-        "description" => $data->Description,
-        "cause" => $data->Cause,
-        "resolution" => $data->Resolution
-      );
-    } else {
-      // Save the Data
-      $this->Data = $data;
+    // Check for Gerneral Errors
+    if (isset($data->Error) && $data->Error < 100) {
+      watchdog("manual_direct_debit_uk", "PCA returns error: " . $data->Description . " – "  . $data->Cause . " – " . $data->Resolution, NULL, WATCHDOG_WARNING);
+      return false;
     }
-  }
 
-  function HasData() {
-    if ( !empty($this->Data) ) {
-      return $this->Data;
-    }
-    return false;
-  }
-
-  function HasError() {
-    if ( !empty($this->Error) ) {
-      return $this->Error;
-    }
-    return false;
+    return $data;
   }
 }
